de9db24ea84bf1e2cfd4f3bcae5c8aad50927f36
Taylan <taylan.isikoglu@gmail.com>
2020-03-10 21:18:07 +0300

    Made changes so when it finds a solution, it prints the steps to the solution aswell

--- eigth_puzzle.cpp
+++ eigth_puzzle.cpp
@@ -10,26 +10,46 @@
 #include <iostream>
 #include <algorithm>
 
+//We use this state object to hold the move list for the correct solution
+struct State {
+	std::vector<int> playground;
+	State* parent;
+
+	State(std::vector<int> playground, State* parent) {
+		this->playground = playground;
+		this->parent = parent;
+	}
+
+	State(State* parent) {
+		this->playground = parent->playground;
+		this->parent = parent;
+	}
+
+	bool operator==(State& rhs) {
+		return this->playground == rhs.playground;
+	}
+};
+
 //Initial state
-std::vector<int> state{2, 8, 3, 1, 6, 4, 7, -1, 5};
+State state({2, 8, 3, 1, 6, 4, 7, -1, 5}, nullptr);
 //The state we are aiming for
-std::vector<int> endState{1, 2, 3, 8, -1, 4, 7, 6, 5};
+State endState({1, 2, 3, 8, -1, 4, 7, 6, 5}, nullptr);
 
 //Holds the states we will discover
 //We use a double ended queue as a FIFO by pushing from front of the queue and popping at the back of the queue 
-std::deque<std::vector<int>> searchQueue;
+std::deque<State*> searchQueue;
 //Holds the states that are already discovered
 //We use a set since searching existing elements is a O(logn) operation
 std::set<std::vector<int>> discoveredSet;
 
 //Prints the vector in 3x3 form on console
-void printVector(std::vector<int>& vec);
+void printVector(State* state);
 
 int main() {
 	std::cout << "Starting..." << std::endl;
 
-	std::vector<int> tmp(state.size());
-	searchQueue.push_front(state);
+	State* tmp;
+	searchQueue.push_front(&state);
 	
 	int step = 0;
 
@@ -38,15 +58,31 @@ int main() {
 		
 		tmp = searchQueue.front();
 		searchQueue.pop_front();
-		discoveredSet.insert(tmp);
+		discoveredSet.insert(tmp->playground);
 
 		//Index of the -1, which represents the empty tile
-		int empty_index = std::distance(tmp.begin(), std::find(tmp.begin(), tmp.end(), -1));
+		int empty_index = std::distance(tmp->playground.begin(), std::find(tmp->playground.begin(), tmp->playground.end(), -1));
 
 		//We reached our goal
-		if (tmp == endState) {
+		if (*tmp == endState) {
 			std::cout << "Program reached its goal in " << step << " steps!" << std::endl;
+			
+			//The solution is a linked list, where the initial state is the last element
+			//To print it in order we need to traverse to the end.
+			std::deque<State*> solution;	
+			while(tmp->parent != nullptr) {
+				solution.push_front(tmp);
+				tmp = tmp->parent;
+			}
+			
+			//Prints the initial state
 			printVector(tmp);
+			//Prints the rest of the solution.
+			while(!solution.empty()) {
+				printVector(solution.front());
+				solution.pop_front();	
+			}
+
 			return 0;
 		}
 		
@@ -66,39 +102,39 @@ int main() {
 		 **/
 		//UP
 		if (empty_index-3 >= 0) {
-			std::swap(tmp.at(empty_index), tmp.at(empty_index-3));
-			if (discoveredSet.count(tmp) != 1 &&
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index-3));
+			if (discoveredSet.count(tmp->playground) != 1 &&
 					std::find(searchQueue.begin(), searchQueue.end(), tmp) == searchQueue.end()) {
-				searchQueue.push_back(tmp);	
+				searchQueue.push_back(new State(tmp));
 			}		
-			std::swap(tmp.at(empty_index), tmp.at(empty_index-3));
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index-3));
 		}
 		//DOWN
-		if (empty_index+3 <= tmp.size()-1) {
-			std::swap(tmp.at(empty_index), tmp.at(empty_index+3));
-			if (discoveredSet.count(tmp) != 1 &&
+		if (empty_index+3 <= tmp->playground.size()-1) {
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index+3));
+			if (discoveredSet.count(tmp->playground) != 1 &&
 					std::find(searchQueue.begin(), searchQueue.end(), tmp) == searchQueue.end()) {
-				searchQueue.push_back(tmp);	
+				searchQueue.push_back(new State(tmp));
 			}		
-			std::swap(tmp.at(empty_index), tmp.at(empty_index+3));	
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index+3));	
 		}
 		//LEFT
 		if (empty_index % 3 != 0) {
-			std::swap(tmp.at(empty_index), tmp.at(empty_index-1));
-			if (discoveredSet.count(tmp) != 1 &&
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index-1));
+			if (discoveredSet.count(tmp->playground) != 1 &&
 					std::find(searchQueue.begin(), searchQueue.end(), tmp) == searchQueue.end()) {
-				searchQueue.push_back(tmp);	
+				searchQueue.push_back(new State(tmp));
 			}		
-			std::swap(tmp.at(empty_index), tmp.at(empty_index-1));
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index-1));
 		}
 		//RIGHT
 		if ((empty_index+1) % 3 != 0) {
-			std::swap(tmp.at(empty_index), tmp.at(empty_index+1));
-			if (discoveredSet.count(tmp) != 1 &&
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index+1));
+			if (discoveredSet.count(tmp->playground) != 1 &&
 					std::find(searchQueue.begin(), searchQueue.end(), tmp) == searchQueue.end()) {
-				searchQueue.push_back(tmp);	
+				searchQueue.push_back(new State(tmp));
 			}		
-			std::swap(tmp.at(empty_index), tmp.at(empty_index+1));
+			std::swap(tmp->playground.at(empty_index), tmp->playground.at(empty_index+1));
 		}
 		//printVector(tmp);
 	}
@@ -107,11 +143,12 @@ int main() {
 	return 0;
 }
 
-void printVector(std::vector<int>& vec) {
-	for (int i = 0; i < vec.size(); i++) {
-		std::cout << vec[i] << " ";
+void printVector(State* state) {
+	for (int i = 0; i < state->playground.size(); i++) {
+		std::cout << state->playground[i] << " ";
 		if ((i+1)%3 == 0 && i != 0) {
 			std::cout << std::endl;
 		}
 	}
+	std::cout << std::endl;
 }
